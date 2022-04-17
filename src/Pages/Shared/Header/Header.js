f8d9901ddf75754f1cea38f0aca5f3407507e3d8
mahfuz47 <mahfuzshikder560@gmail.com>
2022-04-18 00:06:58 +0600

    nav section added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,59 @@
+// import { signOut } from "firebase/auth";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+// import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+// import auth from "../../../firebase.init";
+import "./Header.css";
+
+const Header = () => {
+  //   const [user] = useAuthState(auth);
+
+  //   const handleSignOut = () => {
+  //     signOut(auth);
+  //   };
+
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="dark"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <h1 className="header-icon fw-bold">YOUR DOC</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="home#services">Services</Nav.Link>
+              <Nav.Link href="home#experts">Experts</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="about">
+                About
+              </Nav.Link>
+              {/* {user ? (
+              <button
+                className="btn btn-link text-white text-decoration-none"
+                //   onClick={handleSignOut}
+              >
+                sign out
+              </button>
+              ) : ( */}
+              <Nav.Link as={Link} to="login">
+                Login
+              </Nav.Link>
+              {/* )} */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
